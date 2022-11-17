d6e8d2541a46a6597810fd4ca96101ab389d18e8
rahul <rcchoudharyrahul017@gmail.com>
2022-11-17 14:18:00 +0530

    save

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require('cors')
 require('dotenv').config()
-const port=process.env.PORT
+const port=process.env.PORT ||4000
 
 
 const bodyParser = require('body-parser')
